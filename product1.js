a5a9a5d2a8d7f72033d712bd6f27cb904fde3203
Reena Thomas <rtthomas@ucsc.edu>
2023-06-28 19:40:48 -0700

    update

--- product1.js
+++ product1.js
@@ -1,26 +1,98 @@
-// Image changer functionality
-function changeImage(imageUrl) {
-  const mainImage = document.getElementById('mainImage');
-  mainImage.src = imageUrl;
+// Retrieve cart items from localStorage
+function getCartItems() {
+  let cartItems = localStorage.getItem('cartItems');
+  if (cartItems) {
+    return JSON.parse(cartItems);
+  } else {
+    return [];
+  }
 }
 
-// Incrementer functionality
-const decrementBtn = document.getElementById('decrement-btn');
-const incrementBtn = document.getElementById('increment-btn');
-const quantityInput = document.getElementById('quantity-input');
+// Store cart items in localStorage
+function storeCartItems(cartItems) {
+  localStorage.setItem('cartItems', JSON.stringify(cartItems));
+}
 
-decrementBtn.addEventListener('click', () => {
-  let quantity = parseInt(quantityInput.value);
+// Add selected quantity to cart
+function addToCart() {
+  const quantityInput = document.getElementById('quantity-input');
+  const quantity = parseInt(quantityInput.value);
+
   if (quantity > 0) {
-    quantity--;
+    const productId = 1; // Assuming the product ID is 1 for this example
+    const cartItem = {
+      id: productId,
+      quantity: quantity,
+    };
+
+    // Retrieve existing cart items from localStorage
+    let cartItems = getCartItems();
+
+    // Check if the product is already in the cart
+    const existingItemIndex = cartItems.findIndex(item => item.id === productId);
+    if (existingItemIndex !== -1) {
+      // Update the quantity if the product already exists in the cart
+      cartItems[existingItemIndex].quantity += quantity;
+    } else {
+      // Add the new product to the cart
+      cartItems.push(cartItem);
+    }
+
+    // Store the updated cart items in localStorage
+    storeCartItems(cartItems);
+
+    // Provide feedback or redirect to the cart page
+    alert(`Added ${quantity} pairs to the cart.`);
   } else {
-    quantity = 0;
+    alert('Please select a valid quantity.');
   }
-  quantityInput.value = quantity.toString();
+}
+
+// Increment button click event handler
+document.getElementById('increment-btn').addEventListener('click', function () {
+  const quantityInput = document.getElementById('quantity-input');
+  let quantity = parseInt(quantityInput.value);
+
+  if (!isNaN(quantity)) {
+    quantity++;
+    quantityInput.value = quantity;
+  }
 });
 
-incrementBtn.addEventListener('click', () => {
+// Decrement button click event handler
+document.getElementById('decrement-btn').addEventListener('click', function () {
+  const quantityInput = document.getElementById('quantity-input');
   let quantity = parseInt(quantityInput.value);
-  quantity++;
-  quantityInput.value = quantity.toString();
+
+  if (!isNaN(quantity) && quantity > 0) {
+    quantity--;
+    quantityInput.value = quantity;
+  }
 });
+
+// Add to cart button click event handler
+document.querySelector('.add-to-cart-btn').addEventListener('click', addToCart);
+
+// Display cart contents on cart.html
+function displayCart() {
+  const cartItems = getCartItems();
+  const cartContainer = document.querySelector('main');
+
+  if (cartItems.length === 0) {
+    cartContainer.innerHTML = '<h2>Your Cart</h2><p>Your cart is currently empty.</p>';
+  } else {
+    let cartHTML = '<h2>Your Cart</h2><ul>';
+
+    for (const item of cartItems) {
+      cartHTML += `<li>Product ID: ${item.id}, Quantity: ${item.quantity}</li>`;
+    }
+
+    cartHTML += '</ul>';
+    cartContainer.innerHTML = cartHTML;
+  }
+}
+
+// Run displayCart when cart.html is loaded
+if (window.location.pathname.includes('cart.html')) {
+  displayCart();
+}
