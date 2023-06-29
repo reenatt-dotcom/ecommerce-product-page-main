44fe2eea637503a55b8c34fcccf04f11ec313fb8
Reena Thomas <rtthomas@ucsc.edu>
2023-06-28 21:25:40 -0700

    update

--- product1.js
+++ product1.js
@@ -1,90 +1,72 @@
 // Retrieve cart items from localStorage
 function getCartItems() {
-  let cartItems = localStorage.getItem('cartItems');
-  if (cartItems) {
-    return JSON.parse(cartItems);
-  } else {
-    return [];
-  }
+  const cartItems = localStorage.getItem('cartItems');
+  return cartItems ? JSON.parse(cartItems) : [];
 }
+
 
 // Store cart items in localStorage
 function storeCartItems(cartItems) {
   localStorage.setItem('cartItems', JSON.stringify(cartItems));
 }
 
+
 // Add selected quantity to cart
-function addToCart() {
-  const quantityInput = document.getElementById('quantity-input');
-  const quantity = parseInt(quantityInput.value);
+function addToCart(quantity) {
+  const productId = 1; // Assuming the product ID is 1 for this example
+  const cartItem = {
+    id: productId,
+    quantity: quantity,
+  };
 
-  if (quantity > 0) {
-    const productId = 1; // Assuming the product ID is 1 for this example
-    const cartItem = {
-      id: productId,
-      quantity: quantity,
-    };
+  // Retrieve existing cart items from localStorage
+  let cartItems = getCartItems();
 
-    // Retrieve existing cart items from localStorage
-    let cartItems = getCartItems();
-
-    // Check if the product is already in the cart
-    const existingItemIndex = cartItems.findIndex(item => item.id === productId);
-    if (existingItemIndex !== -1) {
-      // Update the quantity if the product already exists in the cart
-      cartItems[existingItemIndex].quantity += quantity;
-    } else {
-      // Add the new product to the cart
-      cartItems.push(cartItem);
-    }
-
-    // Store the updated cart items in localStorage
-    storeCartItems(cartItems);
-
-    // Provide feedback or redirect to the cart page
-    alert(`Added ${quantity} pairs to the cart.`);
+  // Check if the product is already in the cart
+  const existingItemIndex = cartItems.findIndex(item => item.id === productId);
+  if (existingItemIndex !== -1) {
+    // Update the quantity if the product already exists in the cart
+    cartItems[existingItemIndex].quantity += quantity;
   } else {
-    alert('Please select a valid quantity.');
+    // Add the new product to the cart
+    cartItems.push(cartItem);
   }
+
+  // Store the updated cart items in localStorage
+  storeCartItems(cartItems);
+
+  // Provide feedback or redirect to the cart page
+  alert(`Added ${quantity} pairs to the cart.`);
 }
 
-// Increment button click event handler
-document.getElementById('increment-btn').addEventListener('click', function () {
-  const quantityInput = document.getElementById('quantity-input');
-  let quantity = parseInt(quantityInput.value);
-
-  if (!isNaN(quantity)) {
-    quantity++;
-    quantityInput.value = quantity;
-  }
-});
-
-// Decrement button click event handler
-document.getElementById('decrement-btn').addEventListener('click', function () {
-  const quantityInput = document.getElementById('quantity-input');
-  let quantity = parseInt(quantityInput.value);
-
-  if (!isNaN(quantity) && quantity > 0) {
-    quantity--;
-    quantityInput.value = quantity;
-  }
-});
-
-// Add to cart button click event handler
-document.querySelector('.add-to-cart-btn').addEventListener('click', addToCart);
 
 // Display cart contents on cart.html
 function displayCart() {
   const cartItems = getCartItems();
-  const cartContainer = document.querySelector('main');
+  const cartContainer = document.getElementById('cart-container');
 
   if (cartItems.length === 0) {
-    cartContainer.innerHTML = '<h2>Your Cart</h2><p>Your cart is currently empty.</p>';
+    cartContainer.innerHTML = '<p>Your cart is empty.</p>';
   } else {
-    let cartHTML = '<h2>Your Cart</h2><ul>';
+    let cartHTML = '<ul>';
 
     for (const item of cartItems) {
-      cartHTML += `<li>Product ID: ${item.id}, Quantity: ${item.quantity}</li>`;
+      const product = getProductById(item.id);
+      const totalPrice = product.price * item.quantity;
+
+      cartHTML += `
+        <li>
+          <div class="product-image">
+            <img src="${product.image}" alt="${product.title}" class="cart-item-image">
+          </div>
+          <div class="product-details">
+            <h3 class="product-title">${product.title}</h3>
+            <p class="product-price">$${product.price.toFixed(2)}</p>
+            <p class="product-quantity">Quantity: ${item.quantity}</p>
+            <p class="product-total-price">Total: $${totalPrice.toFixed(2)}</p>
+          </div>
+        </li>
+      `;
     }
 
     cartHTML += '</ul>';
@@ -92,7 +74,77 @@ function displayCart() {
   }
 }
 
-// Run displayCart when cart.html is loaded
-if (window.location.pathname.includes('cart.html')) {
-  displayCart();
+
+
+
+// Retrieve the product title based on its ID
+function getProductTitle(productId) {
+  // Replace this with your implementation to fetch the product title based on the ID
+  // For example, you can have a predefined array or object that maps product IDs to titles
+
+  // Example implementation using a product map
+  const productMap = {
+    1: 'Stylish High Heels',
+    2: 'Elegant Pumps',
+    // Add more mappings for other product IDs
+  };
+
+  // Return the title based on the product ID
+  return productMap[productId] || 'Unknown Product';
+}
+
+
+// Retrieve the product price based on its ID
+function getProductPrice(productId) {
+  let price = 0;
+
+  // Replace the switch statement with your own logic to retrieve the product price
+  switch (productId) {
+    case 1:
+      price = 125.00;
+      break;
+    case 2:
+      price = 99.00;
+      break;
+    // Add more cases for other product IDs
+
+    default:
+      price = 0;
+  }
+
+  return price;
+}
+
+
+// Retrieve the product image based on its ID
+function getProductImage(productId) {
+  let imageUrl = '';
+
+  // Replace the switch statement with your own logic to retrieve the product image URL
+  switch (productId) {
+    case 1:
+      imageUrl = 'images/shoe1/1.webp';
+      break;
+    case 2:
+      imageUrl = 'images/shoe2/1.webp';
+      break;
+    // Add more cases for other product IDs
+
+    default:
+      imageUrl = '';
+  }
+
+  return imageUrl;
+}
+// Get product details by ID
+function getProductById(productId) {
+  // Replace the code below with your actual implementation
+  // Here, we are returning some example data
+  const products = [
+    { id: 1, title: 'Stylish High Heels', price: 125, image: 'images/shoe1/1.webp' },
+    { id: 2, title: 'Elegant Pumps', price: 99, image: 'images/shoe2/1.webp' },
+    // Add more products as needed
+  ];
+
+  return products.find(product => product.id === productId);
 }
