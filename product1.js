f3ce1c07c664577ded6b031413697f53153de8eb
Reena Thomas <rtthomas@ucsc.edu>
2023-06-30 20:20:41 -0700

    remove from cart

--- product1.js
+++ product1.js
@@ -3,35 +3,42 @@ function getCartItems() {
   return cartItems ? JSON.parse(cartItems) : [];
 }
 
+
 function storeCartItems(cartItems) {
   localStorage.setItem('cartItems', JSON.stringify(cartItems));
 }
 
+
 function addToCart(quantity) {
-  const productId = 1; 
+  const productId = 1;
   const cartItem = {
     id: productId,
     quantity: quantity,
   };
 
+
   let cartItems = getCartItems();
 
-  
+
+ 
   const existingItemIndex = cartItems.findIndex(item => item.id === productId);
   if (existingItemIndex !== -1) {
-    
+   
     cartItems[existingItemIndex].quantity += quantity;
   } else {
-    
+   
     cartItems.push(cartItem);
   }
 
-  
+
+ 
   storeCartItems(cartItems);
 
-  
+
+ 
   alert(`Added ${quantity} pairs to the cart.`);
 }
+
 
 function displayCart() {
   const cartItems = getCartItems();
@@ -56,6 +63,7 @@ function displayCart() {
             <p class="product-price">$${product.price.toFixed(2)}</p>
             <p class="product-quantity">Quantity: ${item.quantity}</p>
             <p class="product-total-price">Total: $${totalPrice.toFixed(2)}</p>
+            <button class="remove-from-cart-btn" data-product-id="${product.id}">Remove</button>
           </div>
         </li>
       `;
@@ -63,11 +71,50 @@ function displayCart() {
 
     cartHTML += '</ul>';
     cartContainer.innerHTML = cartHTML;
+
+    // Add event listeners to the remove buttons
+    const removeButtons = document.querySelectorAll('.remove-from-cart-btn');
+    removeButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        const productId = parseInt(button.getAttribute('data-product-id'));
+        removeFromCart(productId);
+      });
+    });
   }
 }
 
+function removeFromCart(productId) {
+  let cartItems = getCartItems();
+
+  // Find the index of the item in the cart
+  const itemIndex = cartItems.findIndex(item => item.id === productId);
+
+  if (itemIndex !== -1) {
+    const currentItem = cartItems[itemIndex];
+
+    if (currentItem.quantity > 1) {
+      // If there is more than one instance of the item, decrement the quantity by one
+      currentItem.quantity -= 1;
+    } else {
+      // If there is only one instance of the item, remove it from the cart
+      cartItems.splice(itemIndex, 1);
+    }
+
+    // Update the cart in local storage
+    storeCartItems(cartItems);
+
+    // Refresh the cart display
+    displayCart();
+
+    alert('Item removed from cart.');
+  }
+}
+
+
+
+
 function getProductTitle(productId) {
-  
+ 
   const productMap = {
     1: 'Stylish High Heels',
     2: 'Elegant Pumps',
@@ -79,17 +126,20 @@ function getProductTitle(productId) {
     8: 'Casual Flats',
     9: 'Ankle Strap Heels',
     10: 'Mary Janes',
-    
+   
   };
 
-  
+
+ 
   return productMap[productId] || 'Unknown Product';
 }
+
 
 function getProductPrice(productId) {
   let price = 0;
 
-  
+
+ 
   switch (productId) {
     case 1:
       price = 125.00;
@@ -121,19 +171,23 @@ function getProductPrice(productId) {
     case 10:
       price = 79.00;
       break;
-    
+   
+
 
     default:
       price = 0.00;
   }
 
+
   return price;
 }
+
 
 function getProductImage(productId) {
   let imageUrl = '';
 
-  
+
+ 
   switch (productId) {
     case 1:
       imageUrl = 'images/shoe1/1.webp';
@@ -165,14 +219,17 @@ function getProductImage(productId) {
     case 10:
       imageUrl = 'images/shoe10/1.jpg';
       break;
-    
+   
+
 
     default:
       imageUrl = '';
   }
 
+
   return imageUrl;
 }
+
 
 function getProductById(productId) {
   const product = {
@@ -182,12 +239,15 @@ function getProductById(productId) {
     image: getProductImage(productId),
   };
 
+
   return product;
 }
+
 
 function handleQuantityChange(increment) {
   const quantityInput = document.getElementById('quantity-input');
   let quantity = parseInt(quantityInput.value);
+
 
   if (increment) {
     quantity += 1;
@@ -197,57 +257,76 @@ function handleQuantityChange(increment) {
     }
   }
 
+
   quantityInput.value = quantity.toString();
 }
 
+
 const incrementBtn = document.getElementById('increment-btn');
 const decrementBtn = document.getElementById('decrement-btn');
+
+
 
 
 incrementBtn.addEventListener('click', function() {
   handleQuantityChange(true);
 });
 
+
 decrementBtn.addEventListener('click', function() {
   handleQuantityChange(false);
 });
 
+
 const addToCartBtn = document.querySelector('.add-to-cart-btn');
 
+
 addToCartBtn.addEventListener('click', function () {
-  
+ 
   const quantityInput = document.getElementById('quantity-input');
 
-  
+
+ 
   const quantity = parseInt(quantityInput.value);
 
-  
+
+ 
   addToCart(quantity);
 });
 
+
 changeImage('images/shoe1/1.webp');
 
+
 function changeImage(imageUrl) {
-  
+ 
   const mainImage = document.getElementById('mainImage');
 
-  
+
+ 
   mainImage.src = imageUrl;
 }
+
+
 
 
 const thumbnailImages = document.querySelectorAll('.thumbnail');
 
 
+
+
 thumbnailImages.forEach(function (thumbnail) {
   thumbnail.addEventListener('click', function () {
-    
+   
     const imageUrl = thumbnail.src;
 
-    
+
+   
     changeImage(imageUrl);
   });
 });
+
+
 
 
 displayCart();
